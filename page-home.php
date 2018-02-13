269fd927261113a4e47248095f2380e49cff94dd
Thomas Krupers <thomaskrupers@gmail.com>
2018-02-13 22:06:29 +0100

    Update home page trackers

--- page-home.php
+++ page-home.php
@@ -1,14 +1,14 @@
 <!-- Xenos Valentijnscampagne -->
 <iframe style="display:none;" src="https://pubads.g.doubleclick.net/gampad/adx?iu=/4045/Blogads/188898404&sz=1x1&t=campaign%3D188898404_2234392795_1&c=123456789" width="1" height="1" border="0"></iframe>
 
-<!-- Becel vegan chocoladeboter -->
-<iframe style="display:none;" src="https://pubads.g.doubleclick.net/gampad/adx?iu=/4045/Blogads/188898404&sz=1x1&t=campaign%3D188898404_2099605589_11&c=123456789" width="1" height="1" border="0"></iframe>
-
-<!-- Becel vegan chocoladeboter -->
-<div style="display:none;"><img src="https://bs.serving-sys.com/serving/adServer.bs?cn=display&c=19&mc=imp&pli=22035507&PluID=0&ord=%%CACHEBUSTER%%&rtu=-1&pcp=$$DCMID$$" border="0" width="1" height="1" /></div>
-
 <!-- Becel Valentijnsspritsen -->
 <iframe style="display:none;" src="https://pubads.g.doubleclick.net/gampad/adx?iu=/4045/Blogads/188898404&sz=1x1&t=campaign%3D188898404_2099605589_12&c=123456789" width="1" height="1" border="0"></iframe>
+
+<!-- Campagne zoetstof is OK artikel 1 -->
+<iframe style="display:none;" src="https://pubads.g.doubleclick.net/gampad/adx?iu=/4045/Blogads/188898404&sz=1x1&t=campaign%3D188898404_2211016246_1&c=123456789" width="1" height="1" border="0"></iframe>
+
+<!-- Campagne zoetstof is OK artikel 1 -->
+<div style="display:none;"><img src="https://track.adform.net/adfserve/?bn=21087872;1x1inv=1;srctype=3;ord=[timestamp]" border="0" width="1" height="1" /></div>
 
 <?php 
   while (have_posts()) : the_post();
